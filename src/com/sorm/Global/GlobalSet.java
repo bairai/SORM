c99f64a823b42cc374d50bd1dfddbc4d5833dfd7
bairai <840429763@qq.com>
2018-03-28 18:13:43 +0800

    Update GlobalSet.java
    看到配置文件突然想起来，这些东西是不能留的

--- src/com/sorm/Global/GlobalSet.java
+++ src/com/sorm/Global/GlobalSet.java
@@ -4,7 +4,7 @@ public class GlobalSet {
     public static final String driver="com.mysql.jdbc.Driver";
     public static final String  url="jdbc:mysql://localhost:3306/sorm?useSSL=true&characterEncoding=UTF-8";
     public static final String user="root";
-    public static final String psw="8404";
+    public static final String psw="";//忘记给删除了！
     public static final String usingDB="mysql";
     public static final String srcPath="C:/Users/Administrator/IdeaProjects/SORM";
     public static final String poPackage="com.sorm.po";
